a7eb5b22b48fe5e928abb982d2bf1f94b2c9fcb3
suchinton <suchintonroxx@gmail.com>
2021-08-17 14:52:56 +0530

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   .vscode/settings.json
    	modified:   prog6/Binary_search
    	modified:   prog6/Binary_search.cpp

--- prog6/Binary_search.cpp
+++ prog6/Binary_search.cpp
@@ -9,8 +9,8 @@ int binarySearch(int A[],int l,int r,int search);
    {
       system("clear");
 
-      cout<<"\n\tBinary_search-[Bubble sort]";
-      cout<<"\n\t--------------------------";
+      cout<<"\n\tBinary_search-[enter sorted elements]";
+      cout<<"\n\t-------------------------------------";
 
       int len, search,result;
       cout<<"\nEnter no. of elements you wish to enter: ";
@@ -25,34 +25,38 @@ int binarySearch(int A[],int l,int r,int search);
          }
 
       system("clear");
-      cout<<"\n\nBefore Sorting: "<<endl;
+      cout<<"\n\nEntered array is: "<<endl;
       for(int i=0;i<len;i++)
          cout<<A[i]<<" ";
-
-      int i,j;
-      for(i=0;i<len-1;i++)
-      for(j=0;j<len-i-1;j++)
-         {
-            if(A[j]>A[j+1])
-               {
-                  int temp=A[j];
-                  A[j]=A[j+1];
-                  A[j+1]=temp;
-               }
-         }
-
-      cout<<"\n\nAfter sorting"<<endl;
-      for(int i=0;i<len;i++)
-         cout<<A[i]<<" "; 
 
       cout<<"\n\nenter the element to search: ";
       cin>>search;
 
       result = binarySearch(A, 0, len - 1, search);
       if(A[result] != search)
-         cout << "Element is not present in array"<<endl;
-      else 
-         cout << "Element is present at position " << result+1<<endl;
+         cout << "\nElement is not present in array"<<endl;
+
+      else if(A[result]==search && A[result+1]==search)
+         {
+            cout<<"\nElement is present at position " << result<<endl;
+            cout<<"\nElement is present at position " << result+1<<endl;
+         }
+
+      else if(A[result]==search && A[result-1]==search)
+         {
+            cout<<"\nElement is present at position " << result<<endl;
+            cout<<"\nElement is present at position " << result+1<<endl;
+         }
+
+      else if(A[result+1]==search && A[result-1]==search && A[result]==search)
+         {
+            cout<<"\nElement is present at position " << result<<endl;
+            cout<<"\nElement is present at position " << result+1<<endl;
+            cout<<"\nElement is present at position " << result+2<<endl;
+            system("exit");
+         }
+      else if(A[result]==search)
+         cout<<"\nElement is present at position " << result+1<<endl;
 
       int ans;
       cout<<"\n\nwould you like to take a screenshot of the output?(Y/n): ";
@@ -67,12 +71,12 @@ int binarySearch(int A[],int l,int r,int search)
       if (r >= l) {
          int mid = l + (r - l) / 2;
 
-         if (A[mid] == search)
-            return mid;
+      if (A[mid] == search)
+         return mid;
 
-         if (A[mid] > search)
-            return binarySearch(A, l, mid - 1, search);
-         return binarySearch(A, mid + 1, r, search);
+      if (A[mid] > search)
+         return binarySearch(A, l, mid - 1, search);
+      return binarySearch(A, mid + 1, r, search);
    }
   
 }
